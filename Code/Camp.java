fd059ab3aa66cf8eff09d988decd05f8cad16482
irene-lijinglin <127362519+irene-lijinglin@users.noreply.github.com>
2023-11-03 13:07:15 +0800

    Junyu did last night
    Haven't modify anything, might need to change to fit the csv or txt file type.

--- Code/Camp.java
+++ Code/Camp.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Camp {
 
 	private String campName;
@@ -7,20 +10,51 @@ public class Camp {
 	private String location;
 	private int totalSlot;
 	private int committeeSlot;
+	private int studentSlot;
 	private String description;
 	private Staff staffInCharge;
-	private Enquiry[] enquiryList;
-	private Suggestion[] suggestionList;
-	private boolean visibility;
+	private List<Enquiry> enquiryList;
+	private List<Suggestion> suggestionList;
+	private List<String> visibility;
+	private List<Student> studentList;
+	private List<CommitteeMember> committeeMemberList;
+	private int currentStudents;
+	private int currentCommitteeMembers;
+	private List<Student> bannedList;
 
+	public Camp(
+		String campName, String location, String description,
+		Date date, Date registrationDate,
+		Faculty userGroup,
+		int committeeSlot, int studentSlot,
+		Staff staffInCharge,
+		List<String> visiblity
+	){
+		this.currentStudents = 0;
+		this.currentCommitteeMembers = 0;
+		this.campName = campName;
+		this.location = location;
+		this.description = description;
+		this.userGroup = userGroup;
+		this.committeeSlot = committeeSlot;
+		this.studentSlot = studentSlot;
+		this.totalSlot = committeeSlot + studentSlot;
+		this.date = date;
+		this.registrationDate = registrationDate;
+		this.staffInCharge = staffInCharge;
+        this.studentList = new ArrayList<>();
+        this.committeeMemberList = new ArrayList<>();
+		this.enquiryList = new ArrayList<>();
+		this.suggestionList = new ArrayList<>();
+		this.bannedList = new ArrayList<>();
+		for (String str : visibility){
+			this.visibility.add(str);
+		}
+	}
 	public String getCampName() {
 		return this.campName;
 	}
 
-	/**
-	 * 
-	 * @param campName
-	 */
 	public void setCampName(String campName) {
 		this.campName = campName;
 	}
@@ -29,10 +63,6 @@ public class Camp {
 		return this.date;
 	}
 
-	/**
-	 * 
-	 * @param date
-	 */
 	public void setDate(Date date) {
 		this.date = date;
 	}
@@ -41,10 +71,6 @@ public class Camp {
 		return this.registrationDate;
 	}
 
-	/**
-	 * 
-	 * @param registrationDate
-	 */
 	public void setRegistrationDate(Date registrationDate) {
 		this.registrationDate = registrationDate;
 	}
@@ -53,10 +79,6 @@ public class Camp {
 		return this.userGroup;
 	}
 
-	/**
-	 * 
-	 * @param userGroup
-	 */
 	public void setUserGroup(Faculty userGroup) {
 		this.userGroup = userGroup;
 	}
@@ -65,10 +87,6 @@ public class Camp {
 		return this.location;
 	}
 
-	/**
-	 * 
-	 * @param Location
-	 */
 	public void setLocation(String Location) {
 		this.location = Location;
 	}
@@ -77,86 +95,143 @@ public class Camp {
 		return this.totalSlot;
 	}
 
-	/**
-	 * 
-	 * @param totalSlot
-	 */
-	public void setTotalSlot(int totalSlot) {
-		this.totalSlot = totalSlot;
+	public int getStudentSlot(){
+		return this.studentSlot;
+	}
+
+	public void setStudentSlot(int studentSlot) {
+		this.studentSlot = studentSlot;
+		this.totalSlot = this.studentSlot + this.committeeSlot;
 	}
 
 	public int getCommitteeSlot() {
 		return this.committeeSlot;
 	}
 
-	/**
-	 * 
-	 * @param committeeSlot
-	 */
 	public void setCommitteeSlot(int committeeSlot) {
 		this.committeeSlot = committeeSlot;
+		this.totalSlot = this.studentSlot + this.committeeSlot;
 	}
 
-	public int getDescription() {
-		// TODO - implement Camp.getDescription
-		throw new UnsupportedOperationException();
+	public String getDescription() {
+		return this.description;
 	}
 
-	/**
-	 * 
-	 * @param Description
-	 */
-	public void setDescription(int Description) {
-		// TODO - implement Camp.setDescription
-		throw new UnsupportedOperationException();
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public Staff getStaffInCharge() {
 		return this.staffInCharge;
 	}
 
-	/**
-	 * 
-	 * @param staffInCharge
-	 */
 	public void setStaffInCharge(Staff staffInCharge) {
 		this.staffInCharge = staffInCharge;
 	}
 
-	public Enquiry[] getEnquiryList() {
-		return this.enquiryList;
+	public void addSuggestion(Suggestion suggestion){
+		this.suggestionList.add(suggestion);
 	}
 
-	/**
-	 * 
-	 * @param enquiryList
-	 */
-	public void setEnquiryList(Enquiry[] enquiryList) {
-		this.enquiryList = enquiryList;
+	public void deleteSuggestion(Suggestion suggestion){
+		this.suggestionList.remove(suggestion);
 	}
 
-	public Suggestion[] getSuggestionList() {
-		return this.suggestionList;
+	public void addEnquiry(Enquiry enquiry){
+		this.enquiryList.add(enquiry);
 	}
 
-	/**
-	 * 
-	 * @param suggestionList
-	 */
-	public void setSuggestionList(Suggestion[] suggestionList) {
-		this.suggestionList = suggestionList;
+	public void deleteEnquiry(Enquiry enquiry){
+		this.enquiryList.remove(enquiry);
 	}
 
-	public boolean getVisibility() {
+	public void addStudent(Student student){
+		if(!bannedList.contains(student)){
+			if(this.currentStudents<this.studentSlot){
+				this.studentList.add(student);
+				this.currentStudents++;
+			}
+			else{
+				System.out.println("This camp is already full\n");
+			}
+		}
+		else{
+			System.out.println("This student is banned");
+		}
+
+	}
+
+	public void deleteStudent(Student student){
+		if(this.currentStudents == 0){
+			System.out.println("No students to delete\n");
+		}
+		else{
+			if(!studentList.contains(student)){
+				System.out.println("This student has not applied to this camp\n");
+			}
+			else{
+				this.studentList.remove(student);
+				this.bannedList.add(student);
+				this.currentStudents--;
+			}
+		}
+	}
+
+	public List<Student> getStudents(){
+		return this.studentList;
+	}
+
+	public void addCommitteeMember(CommitteeMember committeeMember){
+		if(this.currentCommitteeMembers<this.committeeSlot){
+			this.committeeMemberList.add(committeeMember);
+			this.currentCommitteeMembers++;
+		}
+		else{
+			System.out.println("Committee for this camps is full\n");
+		}
+	}
+
+	public void getAllInformation(){
+		System.out.println(
+			"Camp name: "+this.campName+"\n"+
+			"Camp Date: "+this.date+"\n"+
+			"Camp Registration closure date "+this.registrationDate+"\n"+
+			"Camp open to faculties: "+this.userGroup+"\n"+
+			"Camp Location: "+this.location+"\n"+
+			"Camp total Slots: "+this.totalSlot+"\n"+
+			"Camp attendee slots: "+this.studentSlot+"\n"+
+			"Camp committeeMember slots: "+this.committeeSlot+"\n"+
+			"Camp description: "+this.description+"\n"+
+			"Camp staff in charge: "+this.staffInCharge+"\n"+
+			"Camp current number of students: "+this.currentStudents+"\n"+
+			"Camp current number of committee members: "+this.currentCommitteeMembers
+		);
+	}
+
+
+
+	public List<String> getVisibility() {
 		return this.visibility;
 	}
 
-	/**
-	 * 
-	 * @param visibility
-	 */
-	public void setVisibility(boolean visibility) {
-		this.visibility = visibility;
+	public void addVisibility(String visibility){
+		if(!this.visibility.contains(visibility)){
+			this.visibility.add(visibility);
+		}
+		else{
+			System.out.println("Camp already has this visibility\n");
+		}
 	}
+
+	public void deleteVisibility(String visibility){
+		if(this.visibility.contains(visibility)){
+			this.visibility.remove(visibility);
+		}
+		else{
+			System.out.println("Camp already does not contain this visibility setting\n")
+		}
+	}
+
+
 
 }
--- Code/CampList.java
+++ Code/CampList.java
@@ -1,32 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CampList {
 
-	private Camp[] campList;
+	private List<Camp> campList;
 
-	public Camp[] getCampList() {
+	public List<Camp> getCampList() {
 		return this.campList;
 	}
 
-	/**
-	 * 
-	 * @param campList
-	 */
-	public void setCampList(Camp[] campList) {
+	public void setCampList(List<Camp> campList) {
 		this.campList = campList;
 	}
 
-	public void viewCampStudent() {
-		// TODO - implement CampList.viewCampStudent
-		throw new UnsupportedOperationException();
+	public void viewCampStudent(Camp camp) {
+		List<Student> students = camp.getStudents(); 
+	
+		if (students != null) {
+			for (Student student : students) {
+				System.out.println(student.getName());
+			}
+		} else {
+			System.out.println("No students in this camp.\n");
+		}
 	}
 
 	public void viewAllCamp() {
-		// TODO - implement CampList.viewAllCamp
-		throw new UnsupportedOperationException();
+		if (this.CampList != null) {
+			for (Camp camp : campList) {
+				System.out.println(camp.getCampName());
+			}
+		} else {
+			System.out.println("No camps currently.\n");
+		}
 	}
 
 	public void filter() {
 		// TODO - implement CampList.filter
 		throw new UnsupportedOperationException();
+	}
+
+	public void addCamp(Camp camp){
+		this.campList.add(camp);
+	}
+
+	public void deleteCamp(Camp camp){
+		this.campList.remove(camp);
 	}
 
 }
