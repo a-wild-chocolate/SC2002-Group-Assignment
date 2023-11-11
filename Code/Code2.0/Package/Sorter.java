52feff025a8883ead9c2fa3963688f52100b12e3
a-wild-chocolate <69574098+a-wild-chocolate@users.noreply.github.com>
2023-11-11 23:11:25 +0800

    Commit Sorter

--- Code/Code2.0/Package/Sorter.java
+++ Code/Code2.0/Package/Sorter.java
@@ -12,9 +12,23 @@ public class Sorter {
 		return newList;
 	}
 
-	public ArrayList<Student> sortByScore(ArrayList<Student> studentList) {
+	public static ArrayList<Student> sortByScore(ArrayList<Student> studentList) {
 		ArrayList<Student> newList=new ArrayList<>(studentList);
 		Collections.sort(newList, Comparator.comparing(Student::getPoint));
+		return newList;
+	}
+	public static ArrayList<Student> sortByStudentType(Camp camp, StudentStatus userType) {
+		ArrayList<Student> newList= new ArrayList<>();
+		if(userType==StudentStatus.Attendee)
+		{
+			newList=new ArrayList<>(camp.getStudentList());
+		}
+		else if(userType==StudentStatus.CommitteeMember)
+		{
+			newList=new ArrayList<>(camp.getCommitteeMemberList());
+		}
+		newList=sortByName(newList);
+
 		return newList;
 	}
 
