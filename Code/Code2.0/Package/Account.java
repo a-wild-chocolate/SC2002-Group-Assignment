8f8d7b350e54b109b1231df4a7c42609d184af31
a-wild-chocolate <69574098+a-wild-chocolate@users.noreply.github.com>
2023-11-11 23:25:24 +0800

    Commit Sorter

--- Code/Code2.0/Package/Account.java
+++ Code/Code2.0/Package/Account.java
@@ -4,33 +4,12 @@ import java.util.List;
 
 public abstract class Account extends AccountInformation {
 
-	protected Searcher searcher;
-	protected Displayer campDisplayer = new Displayer();
-
 
 	public abstract void ViewCampList();
 
 	public Account(String userID, String name,  String faculty) {
 		// TODO - implement Account.Account
 		super(userID,name,faculty);
-	}
-
-	public void printInformation() {
-		// TODO - implement Account.printInformation
-		System.out.println(
-				"Student name: "+this.campName+"\n"+
-						"Camp Date: "+this.date+"\n"+
-						"Camp Registration closure date "+this.registrationDate+"\n"+
-						"Camp open to faculties: "+this.userGroup+"\n"+
-						"Camp Location: "+this.location+"\n"+
-						"Camp total Slots: "+this.totalSlot+"\n"+
-						"Camp attendee slots: "+this.studentSlot+"\n"+
-						"Camp committeeMember slots: "+this.committeeSlot+"\n"+
-						"Camp description: "+this.description+"\n"+
-						"Camp staff in charge: "+this.staffInCharge+"\n"+
-						"Camp current number of students: "+this.currentStudents+"\n"+
-						"Camp current number of committee members: "+this.currentCommitteeMembers
-		);
 	}
 
 }
