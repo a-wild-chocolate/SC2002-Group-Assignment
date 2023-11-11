de43e3fb32224e4ef56488e1ce22dacb29f92f94
a-wild-chocolate <69574098+a-wild-chocolate@users.noreply.github.com>
2023-11-11 22:51:28 +0800

    Commit Sorter

--- Code/Code2.0/Package/Sorter.java
+++ Code/Code2.0/Package/Sorter.java
@@ -1,15 +1,21 @@
 package Package;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class Sorter {
 
-	public ArrayList<Student> sortByName() {
-		// TODO - implement Sorter.sortByName
-		throw new UnsupportedOperationException();
+	public static ArrayList<Student> sortByName(ArrayList<Student> studentList) {
+		ArrayList<Student> newList=new ArrayList<>(studentList);
+		Collections.sort(newList, Comparator.comparing(Student::getName));
+		return newList;
 	}
 
-	public ArrayList<Student> sortByScore() {
-		// TODO - implement Sorter.sortByScore
-		throw new UnsupportedOperationException();
+	public ArrayList<Student> sortByScore(ArrayList<Student> studentList) {
+		ArrayList<Student> newList=new ArrayList<>(studentList);
+		Collections.sort(newList, Comparator.comparing(Student::getPoint));
+		return newList;
 	}
 
 }
