f5204a264ecb2a8812feb36e160eeb7dd201a91f
a-wild-chocolate <69574098+a-wild-chocolate@users.noreply.github.com>
2023-11-12 01:04:41 +0800

    Commit Sorter add comments

--- Code/Code2.0/Package/RestrictedDisplay.java
+++ Code/Code2.0/Package/RestrictedDisplay.java
@@ -1,14 +1,32 @@
 package Package;
 
+import java.util.ArrayList;
+
 public class RestrictedDisplay extends Displayer {
 
 	/**
 	 * 
 	 * @param campList
 	 */
-	public void display(CampList campList) {
-		// TODO - implement RestrictedDisplay.display
-		throw new UnsupportedOperationException();
+	public void display(ArrayList<Camp> campList,Account account) {
+		ArrayList<Camp> avaliableVisitCamps = new ArrayList<>();
+		for(Camp c:campList)
+		{
+			if(c.getVisibility()==true&&c.getUserGroup().contains(account.getFaculty()))
+			{
+				avaliableVisitCamps.add(c);
+			}
+		}
+		if(avaliableVisitCamps!=null){
+			for(Camp camp:avaliableVisitCamps)
+			{
+				camp.printAllInformation();
+			}
+		}
+		else {
+			System.out.println("There is no camp in the camp list now.");
+		}
+
 	}
 
 }
