ca1f64f9a3a8b5e2721da0d92c81eb58d08047ca
a-wild-chocolate <69574098+a-wild-chocolate@users.noreply.github.com>
2023-11-18 14:32:23 +0800

    seperate suggestion with message. Suggestion now is an independent class. Complete the basic part of suggestion.

--- Code/Code2.0/Package/Suggestion.java
+++ Code/Code2.0/Package/Suggestion.java
@@ -1,21 +1,88 @@
 package Package;
 
-public class Suggestion extends Message {
 
+
+public class Suggestion //extends Message
+{
+
+// attributes
+	private String content;
+	private CommitteeMember sender;
+	private Camp camp;
 	private SuggestionStatus status;
+	private Staff dealer;
 
-	public SuggestionStatu getStatus() {
-		// TODO - implement Suggestion.getStatus
-		throw new UnsupportedOperationException();
+
+// constructor
+	private Suggestion(String content,CommitteeMember sender,Camp camp)
+	{
+		this.content=content;
+		this.sender=sender;
+		this.camp=camp;
+		this.dealer=null;
+		this.status=SuggestionStatus.pending;
 	}
 
+//get and set methods
+//content
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+//sender
+	public CommitteeMember getSender() {
+		return sender;
+	}
+
+	public void setSender(CommitteeMember sender) {
+		this.sender = sender;
+	}
+//camp
+	public Camp getCamp() {
+		return camp;
+	}
+
+	public void setCamp(Camp camp) {
+		this.camp = camp;
+	}
+//dealer
+	public Staff getDealer() {
+		return dealer;
+	}
+
+	public void setDealer(Staff dealer) {
+		this.dealer = dealer;
+	}
+//status
+	public SuggestionStatus getStatus() {
+		return this.status;
+	}
 	/**
-	 * 
+	 *
 	 * @param status
 	 */
-	public void setStatus(SuggestionStatu status) {
-		// TODO - implement Suggestion.setStatus
-		throw new UnsupportedOperationException();
+	public void setStatus(SuggestionStatus status) {
+		this.status=status;
+	}
+
+//methods
+//print the conntent of the suggestion
+	public void viewSuggestion()
+	{
+		System.out.println("Suggestion content: "+this.getContent());
+		System.out.println("Suggestion sender: "+this.sender.getName());
+		System.out.println("Suggestion relate camp: "+this.getCamp().getCampName());
+		System.out.println("Suggestion status: "+this.getStatus());
+		if(this.getStatus()!=SuggestionStatus.pending)
+		{
+			System.out.println("Suggestion dealer: "+this.getDealer().getName());
+			System.out.println("Suggestion dealer's email: "+this.getDealer().getUserID()+"@e.ntu.edu.sg");
+		}
+
+
 	}
 
 }
