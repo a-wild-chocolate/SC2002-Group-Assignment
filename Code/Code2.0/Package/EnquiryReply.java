8bd6bfa8d3f5a48b99a16306c23164d6f402ebe3
a-wild-chocolate <69574098+a-wild-chocolate@users.noreply.github.com>
2023-11-25 23:09:32 +0800

    update EnquiryReply.java

--- Code/Code2.0/Package/EnquiryReply.java
+++ Code/Code2.0/Package/EnquiryReply.java
@@ -2,20 +2,27 @@ package Package;
 
 public class EnquiryReply {
     private int replyId;
-
-
-
     private String content;
-
+    private static int replyNumber=0;
 
     private CommitteeMember replier;
+    //for user
     public EnquiryReply(CommitteeMember replier,String content)
     {
         this.content=content;
         this.replier=replier;
+        replyNumber++;
+        this.replyId=replyNumber;
+    }
+    // for csv
+    public EnquiryReply(int replyId, CommitteeMember replier,String content)
+    {
+        this.replyId=replyId;
+        this.replier=replier;
+        this.content=content;
     }
 
-
+    public int getReplyId() {return replyId;}
     public String getContent() {
         return content;
     }
