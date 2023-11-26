4fac6b3e28197213e967ba6e9a1cf053e6694672
a-wild-chocolate <69574098+a-wild-chocolate@users.noreply.github.com>
2023-11-26 14:25:28 +0800

    debug

--- Code/Code2.0/Package/SorterDisplay.java
+++ Code/Code2.0/Package/SorterDisplay.java
@@ -1,21 +1,11 @@
 package Package;
-import java.time.LocalDate;
+//import java.time.LocalDate;
 import java.util.ArrayList;
-/*
-import Package.CampList;
-import Package.Displayer;
-import Package.PerformanceReport;
-import Package.RestrictedDisplay;
-import Package.SortCampByName;
-import Package.Enquiry;
-import Package.Sorter;
-import Package.Student;
-import Package.Report;
-*/
+
 import java.util.Scanner;
 
 
-public class SorterDisplay extends Displayer{
+public class SorterDisplay{
     public static ArrayList<Student> displaySortingMethod(ArrayList<Student> studentList, Report report) {
 		
         System.out.println("----- Sorting Methods -----");
@@ -34,19 +24,18 @@ public class SorterDisplay extends Displayer{
 			case 1:
 				ArrayList<Student> sortedStudentNameList = Sorter.sortByName(studentList); // Sort the list
                 return sortedStudentNameList; // Return the sorted list
-                break;
 			case 2:
 				ArrayList<Student> sortedStudentScoreList = Sorter.sortByScore(studentList); // Sort the list
                 return sortedStudentScoreList; // Return the sorted list
-				break;
 			case 3:
 				ArrayList<Student> sortedStudentRoleList = Sorter.sortByRole(studentList); // Sort the list
                 return sortedStudentRoleList; // Return the sorted list
-				break;
 			case 0:
 				break;
 			default:System.out.println("Invalid Input!!!");
 		}
+
+		return null;
 	}
 
 	public static ArrayList<Enquiry> displaySortingEnquiryList(ArrayList<Enquiry> enquiriesList) {
@@ -63,22 +52,22 @@ public class SorterDisplay extends Displayer{
 		switch (choice)
 		{
 			case 1:
-				ArrayList<Enquiry> sortedSenderNameList = Sorter.sortByName(enquiriesList); // Sort the list
+				ArrayList<Enquiry> sortedSenderNameList = Sorter.sortBySenderName(enquiriesList); // Sort the list
                 return sortedSenderNameList; // Return the sorted list
-                break;
+
 			case 2:
 				ArrayList<Enquiry> sortedEnquiryIDList = Sorter.sortByID(enquiriesList); // Sort the list
                 return sortedEnquiryIDList; // Return the sorted list
-				break;
+
 			case 3:
 				ArrayList<Enquiry> sortedEnquiryStatusList = Sorter.sortByStatus(enquiriesList); // Sort the list
                 return sortedEnquiryStatusList; // Return the sorted list
-				break;
+
 			case 0:
 				break;
 			default:System.out.println("Invalid Input!!!");
 		}
 
-
+		return null;
 	}
 }
