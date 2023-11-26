563402f7024bb937e81812bd9a5711590793d397
a-wild-chocolate <69574098+a-wild-chocolate@users.noreply.github.com>
2023-11-26 16:10:31 +0800

    create CAMsApp

---
+++ Code/Code2.0/Package/CAMsApp.java
@@ -0,0 +1,52 @@
+package Package;
+
+import java.util.Scanner;
+
+public class CAMsApp {
+    public static void main(String[] args)
+    {
+        LoginAccount loginAccountS;
+        String id;
+        String password;
+        int choice;
+        Account user;
+        Scanner sc = new Scanner(System.in);
+        while(true)
+        {
+            System.out.println();
+            System.out.println("===Camp Application and Management System===");
+            System.out.println("1) Login");
+            System.out.println("0) Quit");
+            choice=sc.nextInt();
+            if(choice==0) return;
+
+            System.out.println("---------Login---------");
+            System.out.println("Enter your ID:");
+            id=sc.nextLine();
+            System.out.println("Enter your password:");
+            password=sc.nextLine();
+            loginAccountS=new LoginAccount(id,password);
+            user=loginAccountS.loginAccount(); //TODO: update loginAccount
+            if(user instanceof Staff)
+            {
+                ((Staff) user).start();
+            }
+            else if(user instanceof Student)
+            {
+
+                while (true)
+                {
+                    System.out.println("Do you want to use attendee account or committee account today?");
+                    System.out.println("1) Attendee account");
+                    System.out.println("2) Committee account");
+                    System.out.println("0) Quit");
+                    choice=sc.nextInt();
+                    if(choice==0) break;
+                    else if(choice==1) (Student)user=createObject; // TODO:get attendee
+                }
+
+            }
+        }
+
+    }
+}
